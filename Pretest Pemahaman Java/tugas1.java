48ffdde7a3e3a46d6169b91e988acb38f041a7a6
alfin11 <ahmadalfinkhoir@gmail.com>
2017-03-05 21:16:14 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/tugas1.java
@@ -0,0 +1,8 @@
+public class tugas1{
+
+     public static void main(String []args){
+         String nama = "Alfin";
+         String kota = "Jepara";
+        System.out.println("Nama : "+nama+" Kota : "+kota);
+     }
+}
